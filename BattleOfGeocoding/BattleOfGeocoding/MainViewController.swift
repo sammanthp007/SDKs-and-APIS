4ff69c119d6a7bbc237da8a4c15130898b0a5968
sammanthp007 <sammanbikramthapa@gmail.com>
2018-09-19 08:35:45 -0700

    Printing on testing, gives error

--- BattleOfGeocoding/BattleOfGeocoding/MainViewController.swift
+++ BattleOfGeocoding/BattleOfGeocoding/MainViewController.swift
@@ -15,6 +15,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.testAddresses()
     }
 
     override func didReceiveMemoryWarning() {
